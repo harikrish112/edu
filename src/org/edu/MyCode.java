2eb262eb5bcb4fab955b91b01419b6df89f819ab
harikrish112 <harikrishnanviswanathan406@gmail.com>
2023-10-18 14:44:19 +0530

    testing

--- src/org/edu/MyCode.java
+++ src/org/edu/MyCode.java
@@ -1,12 +1,12 @@
 package org.edu;
 
 public class MyCode {
-	
-	private void stand1() {
+
+	private void test1() {
 		// TODO Auto-generated method stub
 
 	}
-	private void stand2() {
+	private void test2() {
 		// TODO Auto-generated method stub
 
 	}
