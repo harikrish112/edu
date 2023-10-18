2c470580c1c08d3dfab176ddb82541912b3f2792
harikrish112 <harikrishnanviswanathan406@gmail.com>
2023-10-18 14:40:40 +0530

    1st code

---
+++ src/org/edu/GitNewUserTest.java
@@ -0,0 +1,14 @@
+package org.edu;
+
+public class GitNewUserTest {
+
+	
+	private void test1() {
+	System.out.println("test are preceived");
+
+	}
+	private void test2() {
+	System.out.println("test2 are yet to be checked");
+
+	}
+}
---
+++ src/org/edu/MyCode.java
@@ -0,0 +1,13 @@
+package org.edu;
+
+public class MyCode {
+	
+	private void stand1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void stand2() {
+		// TODO Auto-generated method stub
+
+	}
+}
