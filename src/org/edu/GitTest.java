7d91c03a2b926fd32f6615d59ef6c13b57c85dd7
harikrish112 <harikrishnanviswanathan406@gmail.com>
2023-10-18 14:02:38 +0530

    new lines of codes are added

---
+++ src/org/edu/GitTest.java
@@ -0,0 +1,18 @@
+package org.edu;
+
+public class GitTest {
+
+	
+	public static void main(String[] args) {
+		System.out.println("test1");
+	
+	
+	
+		
+	}
+	private void class2() {
+		System.out.println("test2");
+	}
+	}
+	
+
